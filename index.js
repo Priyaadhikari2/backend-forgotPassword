0398042622b6db7f29d3e6649c4adae4812c835b
Priya Adhikari <150262437+Priyaadhikari2@users.noreply.github.com>
2025-02-01 14:02:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require("express");
-const cors = require("cors");
-const connection = require("./db/connect");
-const userRouter = require("./routes/userRoutes");
-const createTables = require("./models/userSchema");
+require('dotenv').config();
+
+import express, { json } from "express";
+import cors from "cors";
+import connection from "./db/connect";
+import userRouter from "./routes/userRoutes";
+import createTables from "./models/userSchema";
 require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(json());
 
 app.use("/api/v1/user", userRouter);
 
